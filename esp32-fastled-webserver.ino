0b0922705a788318d8ded7784f01f534a5c496d4
Louis Beaudoin <louis@embedded-creations.com>
2018-05-01 11:53:48 +0100

    merged in SmartMatrix FastLED_Functions example, only works until WiFi connects and then crashes

--- esp32-fastled-webserver.ino
+++ esp32-fastled-webserver.ino
@@ -22,6 +22,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <SmartMatrix3.h>
 #include <FastLED.h>
 #include <WiFi.h>
 #include <ESP8266WebServer.h>
@@ -32,6 +33,26 @@
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001008)
 #warning "Requires FastLED 3.1.8 or later; check github for latest code."
 #endif
+
+#define COLOR_DEPTH 24                  // This sketch and FastLED uses type `rgb24` directly, COLOR_DEPTH must be 24
+const uint8_t kMatrixWidth = 32;        // known working: 32, 64, 96, 128
+const uint8_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
+const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
+const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
+const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
+
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
+
+// The 32bit version of our coordinates
+static uint16_t x;
+static uint16_t y;
+static uint16_t z;
+
 
 WebServer webServer(80);
 
@@ -48,12 +69,13 @@ uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 uint8_t power = 1;
 uint8_t brightness = 8;
 
-uint8_t speed = 30;
+uint8_t speed = 20;
 
 // COOLING: How much does the air cool as it rises?
 // Less cooling = taller flames.  More cooling = shorter flames.
 // Default 50, suggested range 20-100
 uint8_t cooling = 50;
+uint16_t scale = 31;
 
 // SPARKING: What chance (out of 255) is there that a new spark will be lit?
 // Higher chance = more roaring fire.  Lower chance = more flickery fire.
@@ -115,6 +137,9 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
   }
 }
 
+// This is the array that we keep our computed noise values in
+uint8_t noise[kMatrixWidth][kMatrixHeight];
+
 void setup() {
   //  delay(3000); // 3 second delay for recovery
   Serial.begin(115200);
@@ -126,16 +151,78 @@ void setup() {
 
   setupWifi();
   setupWeb();
+
+  matrix.addLayer(&backgroundLayer); 
+  matrix.addLayer(&scrollingLayer); 
+  matrix.begin();
+
+  backgroundLayer.setBrightness(128);
+
+  // Initialize our coordinates to some random values
+  x = random16();
+  y = random16();
+  z = random16();
+
+  // Show off smart matrix scrolling text
+  scrollingLayer.setMode(wrapForward);
+  scrollingLayer.setColor({0xff, 0xff, 0xff});
+  scrollingLayer.setSpeed(15);
+  scrollingLayer.setFont(font6x10);
+  scrollingLayer.start("SmartMatrix & FastLED", -1);
+  scrollingLayer.setOffsetFromTop((kMatrixHeight/2) - 5);
+
+}
+
+// Fill the x/y array of 8-bit noise values using the inoise8 function.
+void fillnoise8() {
+  for(int i = 0; i < kMatrixWidth; i++) {
+    int ioffset = scale * i;
+    for(int j = 0; j < kMatrixHeight; j++) {
+      int joffset = scale * j;
+      noise[i][j] = inoise8(x + ioffset,y + joffset,z);
+    }
+  }
+  z += speed;
 }
 
 void loop()
 {
+  static uint8_t circlex = 0;
+  static uint8_t circley = 0;
+
   handleWeb();
 
   if (power == 0) {
 
   }
   else {
+    // if sketch uses swapBuffers(false), wait to get a new backBuffer() pointer after the swap is done:
+    while(backgroundLayer.isSwapPending());
+
+    rgb24 *buffer = backgroundLayer.backBuffer();
+
+    static uint8_t ihue=0;
+    fillnoise8();
+    for(int i = 0; i < kMatrixWidth; i++) {
+      for(int j = 0; j < kMatrixHeight; j++) {
+        // We use the value at the (i,j) coordinate in the noise
+        // array for our brightness, and the flipped value from (j,i)
+        // for our pixel's hue.
+        buffer[kMatrixWidth*j + i] = CRGB(CHSV(noise[j][i],255,noise[i][j]));
+
+        // You can also explore other ways to constrain the hue used, like below
+        // buffer[kMatrixHeight*j + i] = CRGB(CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]));
+      }
+    }
+    ihue+=1;
+
+    backgroundLayer.fillCircle(circlex % kMatrixWidth,circley % kMatrixHeight,6,CRGB(CHSV(ihue+128,255,255)));
+    circlex += random16(2);
+    circley += random16(2);
+
+    // buffer is filled completely each time, use swapBuffers without buffer copy to save CPU cycles
+    backgroundLayer.swapBuffers(false);
+    //matrix.countFPS();      // print the loop() frames per second to Serial
 
   }
 }
